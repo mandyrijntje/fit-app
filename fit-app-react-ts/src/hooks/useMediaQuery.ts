8fecebc37d265ad8060b7f4c258f80d66d3059db
mandyrijntje <msmandym@protonmail.com>
2025-02-08 23:11:17 +0100

    add media query hook

---
+++ fit-app-react-ts/src/hooks/useMediaQuery.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches,
+  );
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    setMatches(media.matches);
+    const listener = (event: MediaQueryListEvent) => setMatches(event.matches);
+    media.addEventListener("change", listener);
+
+    return () => media.removeEventListener("change", listener);
+  }, [query]);
+
+  return matches;
+};
+
+export default useMediaQuery;
