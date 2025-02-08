809447cf80c43c04b352d1f31eeca471e8fe38c5
mandyrijntje <msmandym@protonmail.com>
2025-02-09 00:02:41 +0100

    add shared button component

--- fit-app-react-ts/src/App.tsx
+++ fit-app-react-ts/src/App.tsx
@@ -1,15 +1,30 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
 function App() {
+  const [isPageTop, setIsPageTop] = useState<boolean>(true);
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home,
   );
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const isTopOfWindow = window.scrollY === 0;
+      setIsPageTop(isTopOfWindow);
+      setSelectedPage(SelectedPage.Home);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [isPageTop]);
+
   return (
     <div className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isPageTop={isPageTop}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
--- fit-app-react-ts/src/scenes/navbar/index.tsx
+++ fit-app-react-ts/src/scenes/navbar/index.tsx
@@ -7,21 +7,22 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
 
 type Props = {
+  isPageTop: boolean;
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ isPageTop, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex justify-between items-center";
-  const isAboveMd = useMediaQuery("(min-width: 1060px)");
-
+  const isAboveMd = useMediaQuery("(min-width: 768px)");
+  const navBg = isPageTop ? "" : "bg-primary-100 drop-shadow";
   const [resize, toggleResize] = useState<boolean>(false);
 
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div className={`${navBg} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
-          <div className={`${flexBetween} w-full gap-16`}>
+          <div className={`${flexBetween} w-full gap-8`}>
             <img alt="logo" src={Logo} />
             {isAboveMd ? (
               <div className={`${flexBetween} w-full`}>
@@ -67,6 +68,43 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
+      {!isAboveMd && resize && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          <div className="flex justify-end p-12">
+            <button onClick={() => toggleResize(!resize)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          <div className="ml-[33%] flex flex-col gap-20 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <p>Sign In</p>
+            <ActionButton
+              setSelectedPage={setSelectedPage}
+              value="Join"
+              page={SelectedPage.Contact}
+            />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
